7b6ac431d54dc1c9c374012728ef9246ea60c3b9
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-08-25 01:00:14 +0600

    reusable code

--- home.js
+++ home.js
@@ -1,30 +1,53 @@
 const pin = 1234;
+//function to get input values
+function getInputvalue(id) {
+  const inputfieldNumber = parseFloat(document.getElementById(id).value);
+  return inputfieldNumber;
+}
+function valueOnly(id) {
+  const inputfieldNumber = document.getElementById(id).value;
+  return inputfieldNumber;
+}
+//function to get innertext
+
+function getInnertext(id) {
+  const element = parseFloat(document.getElementById(id).innerText);
+  return element;
+}
+
+//function to set innertext
+
+function setInnertext(value) {
+  document.getElementById("available-bal").innerText = value;
+}
 
 document.getElementById("addMoney-btn").addEventListener("click", function (e) {
   e.preventDefault(); // stops form from refreshing page
 
   const selectBank = document.getElementById("select-bank").value;
-  const accNumber = document.getElementById("acc-number").value;
+  const accNumber = valueOnly("acc-number");
+
+  const getAmount = getInputvalue("amountAdd");
+  console.log(getAmount);
+  const pinNumber = getInputvalue("add-pin");
+
+  const getavailableBalance = getInnertext("available-bal");
+
+  const newBalance = getAmount + getavailableBalance;
+  // document.getElementById("available-bal").innerText = newBalance;
+
+  //  call setinnerText function
+  setInnertext(newBalance);
 
   if (accNumber.length < 11) {
     alert("Please Provide Correct Account Number");
     return;
   }
-  const pinNumber = document.getElementById("add-pin").value;
-  //   console.log(selectBank, accNumber, pinNumber);
+
   if (pinNumber != pin) {
     alert("Please Provide the valid pin");
     return;
   }
-  const getAmount = parseFloat(document.getElementById("amountAdd").value);
-
-  const availableBalance = document.getElementById("available-bal");
-  const getavailableBalance = parseFloat(
-    document.getElementById("available-bal").innerText
-  );
-
-  const newBalance = getAmount + getavailableBalance;
-  availableBalance.innerText = newBalance;
 });
 
 //toggling
