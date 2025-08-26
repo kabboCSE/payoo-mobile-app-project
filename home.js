bfef55e859acea22c17a9a5eba33bf35d10545bb
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-08-26 22:54:41 +0600

    functionality complete

--- home.js
+++ home.js
@@ -45,6 +45,7 @@ function handleBtn(id) {
     .classList.add("border-[#0874f2]", "bg-[#0874f20d]");
 }
 
+// add money
 document.getElementById("addMoney-btn").addEventListener("click", function (e) {
   e.preventDefault(); // stops form from refreshing page
 
@@ -82,57 +83,6 @@ document.getElementById("addMoney-btn").addEventListener("click", function (e) {
   }
 });
 
-//toggling
-//add money
-document
-  .getElementById("add-money-logo")
-  .addEventListener("click", function () {
-    handleToggle("addMoney");
-    handleBtn("add-money-logo");
-
-    // btn-background
-    // const forms = document.getElementsByClassName("form-btn");
-    // for (const btn of forms) {
-    //   btn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
-    //   btn.classList.add("border-gray-300");
-    // }
-    // document
-    //   .getElementById("add-money-logo")
-    //   .classList.remove("border-gray-300");
-
-    // document
-    //   .getElementById("add-money-logo")
-    //   .classList.add("border-[#0874f2]", "bg-[#0874f20d]");
-  });
-//cashout
-document.getElementById("cash-out-logo").addEventListener("click", function () {
-  handleToggle("cashout-form");
-  handleBtn("cash-out-logo");
-});
-//transfer
-document
-  .getElementById("transfer-button")
-  .addEventListener("click", function () {
-    handleToggle("transfer-form");
-    handleBtn("transfer-button");
-  });
-// Get Bonus
-document.getElementById("getBonus-btn").addEventListener("click", function () {
-  handleToggle("getBonus-form");
-  handleBtn("getBonus-btn");
-});
-// Pay Bill
-document.getElementById("payBill-btn").addEventListener("click", function () {
-  handleToggle("payBill");
-  handleBtn("payBill-btn");
-});
-// Transactions
-document
-  .getElementById("transaction-btn")
-  .addEventListener("click", function () {
-    handleToggle("transactions-parent");
-    handleBtn("transaction-btn");
-  });
 //cashout
 document.getElementById("withdraw-btn").addEventListener("click", function (e) {
   e.preventDefault();
@@ -179,7 +129,121 @@ document.getElementById("withdraw-btn").addEventListener("click", function (e) {
     return;
   }
 });
-//transfer money
-document.getElementById("agent-number").addEventListener("click", function (e) {
+
+document
+  .getElementById("transaction-btn")
+  .addEventListener("click", function () {
+    console.log("tran btn Clciked");
+  });
+
+// Transfer Money
+document.getElementById("sendNow-btn").addEventListener("click", function (e) {
   e.preventDefault();
+  //amount withdraw
+
+  const accNum = valueOnly("user-number");
+
+  if (accNum.length < 11) {
+    alert("Account Number should be over 10 digit");
+    return;
+  }
+
+  const amountTransfer = getInputvalue("amountTransfer");
+
+  if (amountTransfer < 11) {
+    alert("Amount should be over 10");
+
+    return;
+  }
+
+  //avai - bal
+  // const availableBalance = document.getElementById("available-bal");
+  const getavailableBalance = getInnertext("available-bal");
+  //   document.getElementById("available-bal").innerText
+  // );
+  const newBalance = getavailableBalance - amountTransfer;
+  setInnertext(newBalance);
+
+  const data = {
+    name: "Transfer Money",
+    date: new Date().toLocaleTimeString(),
+  };
+
+  transactionData.push(data);
+  console.log(transactionData);
+
+  if (newBalance < 0) {
+    alert("You dont have balance");
+    setInnertext(0);
+    return;
+  }
 });
+
+//Transaction
+document
+  .getElementById("transaction-btn")
+  .addEventListener("click", function () {
+    const transactionContainer = document.getElementById(
+      "transaction-container"
+    );
+    transactionContainer.innerText = "";
+    for (const data of transactionData) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+        <div class="bg-white rounded-x mt-4 p-3 flex justify-between items-center">
+          <div class="flex gap-4 items-center">
+            <div class="p-3  rounded-full bg-[#f4f5f7]">
+              <img src="./assets/wallet1.png " alt="" />
+            </div>
+
+            <div>
+              <h1>${data.name}</h1>
+              <p>${data.date}</p>
+            </div>
+          </div>
+
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+        `;
+      transactionContainer.appendChild(div);
+    }
+  });
+
+//toggling start
+//add money
+document
+  .getElementById("add-money-logo")
+  .addEventListener("click", function () {
+    handleToggle("addMoney");
+    handleBtn("add-money-logo");
+  });
+//cashout
+document.getElementById("cash-out-logo").addEventListener("click", function () {
+  handleToggle("cashout-form");
+  handleBtn("cash-out-logo");
+});
+//transfer
+document
+  .getElementById("transfer-button")
+  .addEventListener("click", function () {
+    handleToggle("transfer-form");
+    handleBtn("transfer-button");
+  });
+// Get Bonus
+document.getElementById("getBonus-btn").addEventListener("click", function () {
+  handleToggle("getBonus-form");
+  handleBtn("getBonus-btn");
+});
+// Pay Bill
+document.getElementById("payBill-btn").addEventListener("click", function () {
+  handleToggle("payBill");
+  handleBtn("payBill-btn");
+});
+// Transactions
+document
+  .getElementById("transaction-btn")
+  .addEventListener("click", function () {
+    handleToggle("transactions-parent");
+    handleBtn("transaction-btn");
+  });
+// toggling end
