79fbb00658519375d13e5b1b7a6b542346cec0f8
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-08-27 02:17:02 +0600

    modified

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 // login button functionality
 document.getElementById("loginButton").addEventListener("click", function (e) {
   e.preventDefault();
-  const mobileNumber = 12345678910;
-  const pinNumber = 1234;
+  const mobileNumber = "01500600900";
+  const pinNumber = "1234"; // make it string
 
-  const mobileNumberValue = document.getElementById("mobile-number").value;
-  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+  const mobileNumberValue = document
+    .getElementById("mobile-number")
+    .value.trim();
+  const pinNumberValue = document.getElementById("pin-number").value.trim();
 
-  const pinNumberValue = document.getElementById("pin-number").value;
-  const pinNumberValueConverted = parseInt(pinNumberValue);
-
-  if (
-    mobileNumberValueConverted === mobileNumber &&
-    pinNumberValueConverted === pinNumber
-  ) {
+  if (mobileNumberValue === mobileNumber && pinNumberValue === pinNumber) {
     window.location.href = "./home.html";
   } else {
     alert("Invalid credentials");
